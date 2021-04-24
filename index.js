a2a1dc7c83e92ec28bb6aadf86d796c60192d512
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-23 22:07:48 -0500

    Setup basic command

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
+
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login(process.env.TOKEN);
+client.on('message', message => {
+    if (message.content.toLowerCase() === 'jon') {
+        message.channel.send(`Did someone say Jon? Which one? There's at least two of them. Maybe more at this point. (Jon Smith is better though)`)
+    }
+    console.log(message.content);
+})
+
+client.login(config.token);
